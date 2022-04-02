d4110b7cdb7a4e150530a17368351d1659634ce8
Minimage <seancampbell914@gmail.com>
2022-04-02 16:03:26 +0000

    DOM Gen is finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "your"];
+  let adj = ["great", "adorable", "aggressive", "delightful", "evil"];
+  let noun = ["earthquake", "poison", "bubble", "cobweb"];
+  let ext = [".com", ".org", ".net", ".us", ".io"];
+  let finished = [];
+
+  for (var x = 0; x < pronoun.length; x++) {
+    for (var y = 0; y < adj.length; y++) {
+      for (var z = 0; z < noun.length; z++) {
+        for (var i = 0; i < ext.length; i++) {
+          finished.push(pronoun[x] + adj[y] + noun[z] + ext[i]);
+        }
+      }
+    }
+  }
+  console.log(finished);
+  document.getElementById("gen").innerHTML = finished
+    .map((item, index) => {
+      return "<div class='domain'>" + item + "</div>";
+    })
+    .join("");
 };
